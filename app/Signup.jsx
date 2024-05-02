89158600dfd802673a00967d48ff23f0ccf25ace
Mohammad Al Fallah <mohammad.alfallah@lau.edu>
2024-05-02 06:51:39 +0300

    Initial commit

---
+++ app/Signup.jsx
@@ -0,0 +1,11 @@
+import { TabBar, Tab, Layout, Text } from '@ui-kitten/components';
+
+const Signup = () => {
+    return (
+    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text category='h1'>Signup</Text>
+    </Layout>
+  );
+}
+
+export default Signup;
